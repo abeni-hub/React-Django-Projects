74e07d9493e90f5d0fac0ad811f51858ca329d2f
abeni-hub <ebenezarsileshi@gmail.com>
2026-02-13 11:58:23 +0300

    add the update realted to daycare expense

--- daycare-expense-system/finance/admin.py
+++ daycare-expense-system/finance/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Account, Category, Invoice, ExpenseItem, Income
 
-# Register your models here.
+
+class ExpenseItemInline(admin.TabularInline):
+    model = ExpenseItem
+    extra = 1
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [ExpenseItemInline]
+
+
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Income)
+admin.site.register(Invoice, InvoiceAdmin)
